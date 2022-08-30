eb65899c9d5e466c5b0e01b79077949cbf7fc1e4
Maya-Alferez <m.alferez17@gmail.com>
2022-08-30 14:19:33 -0500

    Proteger ruta de citas
    Si el usuario no esta autenticado, este no podrá añadir una cita a la BD, se crea una función para que lo lleve a la página principal e inicie sesión.

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -6,10 +6,11 @@ use MVC\Router;
 
 class CitaController {
     public static function index(Router $router) {
-
         if(!isset($_SESSION['nombre'])) {
             header('Location: /');
         }
+        
+        isAuth();
 
         $router->render('cita/index', [
             'nombre' => $_SESSION['nombre'],
--- includes/funciones.php
+++ includes/funciones.php
@@ -12,3 +12,10 @@ function s($html) : string {
     $s = htmlspecialchars($html);
     return $s;
 }
+
+//Función que revisa que el usuario este autenticado
+function isAuth() : void {
+    if(!isset($_SESSION['login'])) {
+        header('Location: /');
+    }
+}
