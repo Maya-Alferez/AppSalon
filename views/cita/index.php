f1331d883652fc0b8260c738013b5fe27d1e3770
Maya-Alferez <m.alferez17@gmail.com>
2022-08-30 14:23:16 -0500

    Mostrar el nombre del cliente
    Se agrega a la vista de citas el nombre del usuario en la parte superior.

--- views/cita/index.php
+++ views/cita/index.php
@@ -1,6 +1,10 @@
 <h1 class="nombre-pagina">Crear nueva cita</h1>
 <p class="descripcion-pagina">Elige tus servicios y coloca tus datos</p>
 
+<div class="barra">
+    <p>Hola: <?php echo $nombre ?? ''; ?> </p>
+</div>
+
 <div id="app">
     <nav class="tabs">
         <button class="actual" type="button" data-paso="1">Servicios</button>
