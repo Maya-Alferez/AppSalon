0e8ba038187c5b8379fb5aab9d2bc2f6822b0cc3
Maya-Alferez <m.alferez17@gmail.com>
2022-09-02 12:18:34 -0500

    Formulario actualizar servicio
    Se crea el formulario en la acción de actualizar.

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -41,13 +41,19 @@ class ServicioController {
     }
 
     public static function actualizar(Router $router) {
+        $id = is_numeric($_GET['id']);
+        if(!$id) return;
+        $servicio = Servicio::find($id);
+        $alertas = [];
+
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
             
         }
 
         $router->render('servicios/actualizar', [
-            'nombre' => $_SESSION['nombre']
-
+            'nombre' => $_SESSION['nombre'],
+            'servicio' => $servicio,
+            'alertas' => $alertas
         ]);
     }
 
--- views/servicios/actualizar.php
+++ views/servicios/actualizar.php
@@ -2,3 +2,9 @@
 <p class="descripcion-pagina">Modifica los valores del formulario</p>
 
 <?php include_once __DIR__ . '/../templates/barra.php'; ?>
+<?php include_once __DIR__ . '/../templates/alertas.php'; ?>
+
+<form method="POST" class="formulario">
+    <?php include_once __DIR__ . '/formulario.php'; ?>
+    <input type="submit" class="boton" value="Actualizar">
+</form>
