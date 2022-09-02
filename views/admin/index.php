dddaf8819b05e99752fca6f683bdc54814d1cd5a
Maya-Alferez <m.alferez17@gmail.com>
2022-09-02 09:46:32 -0500

    Eliminar citas
    Se crea el método de eliminar en el APIController, para que el admin pueda eliminar citas de cualquier fecha.

--- views/admin/index.php
+++ views/admin/index.php
@@ -28,27 +28,31 @@
                 if($idCita !== $cita->id) { 
                     $total = 0;         
         ?>
-        <li>
-            <p>ID: <span><?php echo $cita->id; ?></span></p>
-            <p>Hora: <span><?php echo $cita->hora; ?></span></p>
-            <p>Cliente: <span><?php echo $cita->cliente; ?></span></p>
-            <p>Email: <span><?php echo $cita->email; ?></span></p>
-            <p>Teléfono: <span><?php echo $cita->telefono; ?></span></p>
+                <li>
+                    <p>ID: <span><?php echo $cita->id; ?></span></p>
+                    <p>Hora: <span><?php echo $cita->hora; ?></span></p>
+                    <p>Cliente: <span><?php echo $cita->cliente; ?></span></p>
+                    <p>Email: <span><?php echo $cita->email; ?></span></p>
+                    <p>Teléfono: <span><?php echo $cita->telefono; ?></span></p>
 
-            <h3>Servicios</h3>
-            <?php
-                $idCita = $cita->id;
-            } //Fin del if 
-                $total += $cita->precio;
-            ?>
-            <p class="servicio"><?php echo $cita->servicio . " $" . $cita->precio; ?></p>
-            <?php
-                $actual = $cita->id;
-                $proximo = $citas[$key +1]->id ?? 0;
-                if(esUltimo($actual, $proximo)) { ?>
-                <p class="total">Total: <span>$ <?php echo $total; ?></span></p>
-            <?php } ?>
-        <?php } //Fin del forEach ?>
+                    <h3>Servicios</h3>
+                    <?php
+                        $idCita = $cita->id;
+                } //Fin del if 
+                        $total += $cita->precio;
+                    ?>
+                    <p class="servicio"><?php echo $cita->servicio . " $" . $cita->precio; ?></p>
+                    <?php
+                        $actual = $cita->id;
+                        $proximo = $citas[$key +1]->id ?? 0;
+                        if(esUltimo($actual, $proximo)) { ?>
+                            <p class="total">Total: <span>$ <?php echo $total; ?></span></p>
+                            <form action="/api/eliminar" method="POST">
+                                <input type="hidden" name="id" value="<?php echo $cita->id; ?>">
+                                <input type="submit" class="boton-eliminar" value="Eliminar">
+                            </form>
+                    <?php }
+            } //Fin del forEach ?>
     </ul>
 </div>
 
