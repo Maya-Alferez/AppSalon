da1ca6d61d83f1b9f985b03dac9a2afcbe36f84c
Maya-Alferez <m.alferez17@gmail.com>
2022-09-02 10:31:35 -0500

    Routing Servicios
    Se crean los métodos, para las nuevas acciones del administrador "ServicioController"

---
+++ controllers/ServicioController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Controllers;
+use MVC\Router;
+
+class ServicioController {
+    public static function index(Router $router) {
+        $router->render('servicios/index', [
+
+        ]);
+    }
+
+    public static function crear(Router $router) {
+        
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        }
+    }
+
+    public static function actualizar(Router $router) {
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            
+        }
+    }
+
+    public static function eliminar(Router $router) {
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            
+        }
+    }
+
+}
