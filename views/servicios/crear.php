85b926aab4e2026537baf103f3b476c28ab51e34
Maya-Alferez <m.alferez17@gmail.com>
2022-09-02 11:24:27 -0500

    Formulario de servicios
    Se crea formulario para crear nuevos servicios.

--- views/servicios/crear.php
+++ views/servicios/crear.php
@@ -2,3 +2,8 @@
 <p class="descripcion-pagina">Llena todos los campos para añadir un nuevo servicio</p>
 
 <?php include_once __DIR__ . '/../templates/barra.php'; ?>
+
+<form action="/servicios/crear" method="POST" class="formulario">
+    <?php include_once __DIR__ . '/formulario.php'; ?>
+    <input type="submit" class="boton" value="Guardar servicio">
+</form>
---
+++ views/servicios/formulario.php
@@ -0,0 +1,9 @@
+<div class="campo">
+    <label for="nombre">Nombre</label>
+    <input type="text" id="nombre" placeholder="Nombre servicio" name="nombre">
+</div>
+
+<div class="campo">
+    <label for="precio">Precio</label>
+    <input type="number" id="precio" placeholder="Precio servicio" name="precio">
+</div>
