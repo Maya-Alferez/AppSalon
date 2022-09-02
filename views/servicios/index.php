1e24b7afbdf51dc9bf7235aa319ae854da03ed0b
Maya-Alferez <m.alferez17@gmail.com>
2022-09-02 12:09:55 -0500

    CSS a los servicios
    Se agregan botones de acciones en la página de servicios.

--- views/servicios/index.php
+++ views/servicios/index.php
@@ -8,6 +8,14 @@
         <li>
             <p>Nombre: <span><?php echo $servicio->nombre; ?></span></p>
             <p>Precio: <span>$<?php echo $servicio->precio; ?></span></p>
+
+            <div class="acciones">
+                <a class="boton" href="/servicios/actualizar?id=<?php echo $servicio->id; ?>">Actualizar</a>
+                <form action="/servicios/eliminar" method="POST">
+                    <input type="hidden" name="id" value="<?php echo $servicio->id; ?>">
+                    <input type="submit" value="Eliminar" class="boton-eliminar">
+                </form>
+            </div>
         </li>
     <?php } //Fin forEach ?>
 </ul>
